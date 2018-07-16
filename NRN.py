0f803ab3e0aa872aa5c07298e2ac5b45d976076a
Kyle Anderson <kyleanderson@u.boisestate.edu>
2018-07-15 23:20:31 -0600

    Cleaned up old unused code. Second page populating is almost done

--- NRN.py
+++ NRN.py
@@ -14,58 +14,18 @@ class Application(tk.Tk):
 
         self.frame = {}
 
-        frame = StartPage(container, self)
+        for F in(StartPage, mainPage):
+            frame = F(container, self)
 
-        self.frame[StartPage] = frame
+            self.frame[F] = frame
 
-        frame.grid(row=0, column=0, sticky="nsew")
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
         frame = self.frame[cont]
         frame.tkraise()
-
-
-
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text = 'Quit', command=self.quit)
-        self.quitButton.grid()
-
-        self.userNameDialog = tk.Entry(master=None)
-        self.userNameDialog.grid()
-
-        self.passwordDialog = tk.Entry(master=None, show="*")
-        self.passwordDialog.grid()
-
-        self.serverDialog = tk.Entry(master=None)
-        self.serverDialog.grid()
-
-        self.testButton = tk.Button(self, text='Create Connection', command=lambda *args: self.assignVariables())
-        self.testButton.grid()
-
-
-    def assignVariables(self):
-        username = self.userNameDialog.get()
-        password = self.passwordDialog.get()
-        servername = self.serverDialog.get()
-
-        self.userNameDialog.delete(0, 'end')
-        self.passwordDialog.delete(0,'end')
-        self.serverDialog.delete(0,'end')
-        try:
-            from plexapi.myplex import MyPlexAccount
-            account = MyPlexAccount(username, password)
-            plex = account.resource(servername).connect()
-            self.plexStatus = tk.Label(master=None, text="Plex Connection Successful")
-            self.plexStatus.grid()
-            self.plexFrame()
-
-        except BaseException:
-            self.plexStatusFail = tk.Label(master=None, text="Plex Connection Unsuccessful. Please try again.")
-            self.plexStatusFail.grid()
-    def plexFrame(self):
-        window = tk.Toplevel(self)
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -93,6 +53,7 @@ class StartPage(tk.Frame):
                 plex = account.resource(servername).connect()
                 plexStatus = tk.Label(master=None, text="Plex Connection Successful")
                 plexStatus.pack()
+                controller.show_frame(mainPage)
 
             except BaseException:
                 plexStatusFail = tk.Label(master=None, text="Plex Connection Unsuccessful. Please try again.")
@@ -100,6 +61,12 @@ class StartPage(tk.Frame):
 
         button1 = tk.Button(self, text="Connect to Server", command=lambda: assignVar())
         button1.pack()
+class mainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Second Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
 
 app = Application()
 app.mainloop()
